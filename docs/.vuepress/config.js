26f81dd10962239df1d2529bc065ab754feae8ed
Karla Jimenez <kvljimenez@gmail.com>
2023-09-27 01:24:59 +0800

    Updating config file

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,13 @@
 module.exports = {
-    title: 'Karla Jimenez',
+    title: 'Excel PivotTable',
     description: 'Excel PivotTable tutorial using vuepress',
-    base: '/'
+    base: '/',
+    themeConfig: {
+      tagline: false,
+      nav: [
+        {text: 'Home', link: '/'},
+        {text: 'Guide', link: '/guide/'},        
+      ],
+      sidebar: 'auto'
+    }
   }
